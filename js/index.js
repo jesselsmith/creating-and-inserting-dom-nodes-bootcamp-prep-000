f286d4f61f58ac767515059e16ae48a1bec052ca
Jesse SMith <jesse.lee.smith@gmail.com>
2019-09-14 17:04:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+
+
+
 document.body.appendChild(element);
 
